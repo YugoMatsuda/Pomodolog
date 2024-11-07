babbf823f5522bad00e0dd66f7dd5ff46eea2727
Yugo Matsuda <yugo1997zazen@gmail.com>
2024-11-07 20:28:09 +0900

    Initial Commit

---
+++ Pomodolog Watch App/PomodologApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodologApp.swift
+//  Pomodolog Watch App
+//
+//  Created by Yugo Matsuda on 2024-11-07.
+//
+
+import SwiftUI
+
+@main
+struct Pomodolog_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Pomodolog/PomodologApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodologApp.swift
+//  Pomodolog
+//
+//  Created by Yugo Matsuda on 2024-11-07.
+//
+
+import SwiftUI
+
+@main
+struct PomodologApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
