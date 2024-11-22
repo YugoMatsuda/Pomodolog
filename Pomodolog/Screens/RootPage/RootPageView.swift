f302fc251b1cd69ecb9dc348053e8b5ef8002aa7
Yugo Matsuda <yugo1997zazen@gmail.com>
2024-11-22 09:58:38 +0900

    Add rootstate

---
+++ Pomodolog/Screens/RootPage/RootPageView.swift
@@ -0,0 +1,18 @@
+//
+//  RootPageView.swift
+//  Pomodolog
+//
+//  Created by Yugo Matsuda on 2024-11-19.
+//
+
+import SwiftUI
+
+struct RootPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RootPageView()
+}
