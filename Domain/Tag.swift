d694c7d59b88932bda20fd75b1e592e357ff0cdc
Yugo Matsuda <yugo1997zazen@gmail.com>
2024-11-24 13:48:49 +0900

    Add defaultTags

--- Domain/Tag.swift
+++ Domain/Tag.swift
@@ -1,4 +1,5 @@
 import Foundation
+import SwiftUI
 
 struct Tag: Identifiable, Equatable, Sendable {
     var id: String
@@ -25,5 +26,52 @@ struct Tag: Identifiable, Equatable, Sendable {
         self.sessionIds = sessionIds
         self.createAt = createAt
         self.updateAt = updateAt
+    }
+}
+
+extension Tag {
+    static func focus() -> Self {
+        .init(
+            id: "focus",
+            name: "🎯 Focus",
+            colorHex: Color.green.toHex() ?? "",
+            sort: 0,
+            sessionIds: [],
+            createAt: .now,
+            updateAt: .now
+        )
+    }
+    
+    static func defaultTags() -> [Tag] {
+        [
+            .focus(),
+            .init(
+                id: "work",
+                name: "💻 Work",
+                colorHex: Color.blue.toHex() ?? "",
+                sort: 1,
+                sessionIds: [],
+                createAt: .now,
+                updateAt: .now
+            ),
+            .init(
+                id: "reading",
+                name: "📖 Reading",
+                colorHex: Color.cyan.toHex() ?? "",
+                sort: 1,
+                sessionIds: [],
+                createAt: .now,
+                updateAt: .now
+            ),
+            .init(
+                id: "study",
+                name: "✏️ Study",
+                colorHex: Color.purple.toHex() ?? "",
+                sort: 1,
+                sessionIds: [],
+                createAt: .now,
+                updateAt: .now
+            )
+        ]
     }
 }
