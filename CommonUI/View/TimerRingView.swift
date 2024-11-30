d9805c8faadc4b3c0d4bc7c2374b665893815fbc
Yugo Matsuda <yugo1997zazen@gmail.com>
2024-11-30 14:39:48 +0900

    Fix animation

--- CommonUI/View/TimerRingView.swift
+++ CommonUI/View/TimerRingView.swift
@@ -15,105 +15,65 @@ struct TimerRingView: View {
     
     var body: some View {
         Group {
-            ZStack {
-                switch param {
-                case .idle(let data):
-                    ZStack {
-                        DotCircleView(timerColor: param.timerColor)
-                            .transition(.scale)
-                            .scaleEffect(1.05)
-
-                        Circle()
-                            .fill(param.timerColor)
-                            .scaleEffect(0.9)
-                        
-                        VStack(spacing: 0) {
+            switch param {
+            case .idle(let data), .workSession(let data):
+                ZStack {
+                    DotCircleView(timerColor: param.timerColor)
+                        .scaleEffect(param.isIdleSession ? 1.05 : 0)
+                    
+                    Circle()
+                        .stroke(innserCircleBackground, lineWidth: 4)
+                        .transition(.scale)
+                        .scaleEffect(param.isWorkSession ? 1.1 : 0)
+                    
+                    Circle()
+                        .trim(from: 0, to: data.progress)
+                        .stroke(param.timerColor.gradient, lineWidth: 4)
+                        .rotationEffect(.init(degrees: 270))
+                        .scaleEffect(param.isWorkSession ? 1.1 : 0)
+                    
+                    Circle()
+                        .fill(param.timerColor)
+                        .frame(width: 30, height: 30)
+                        .overlay(content: {
+                            Circle()
+                                .fill(.white)
+                                .padding(5)
+                        })
+                        .offset(x: circleSize / 2)
+                        .rotationEffect(.init(degrees: (data.progress * 360) + 270.0))
+                        .scaleEffect(param.isWorkSession ? 1.1 : 0)
+                    
+                    Circle()
+                        .fill(data.progress >= 1 ? param.timerColor : innserCircleBackground)
+                        .overlay {
+                            if 0 < data.progress && data.progress < 1 {
+                                Wave(offset: Angle(degrees: self.waveOffset.degrees), ratio: data.progress)
+                                    .fill(param.timerColor.gradient.opacity(0.8))
+                                    .mask {
+                                        Circle()
+                                    }
+                                
+                                Wave(offset: Angle(degrees: self.waveOffset2.degrees), ratio: data.progress)
+                                    .fill(param.timerColor.opacity(0.5))
+                                    .mask {
+                                        Circle()
+                                    }
+                            }
+                        }
+                        .scaleEffect(0.9)
+                    
+                    VStack(spacing: 0) {
+                        if param.isIdleSession {
                             Text(data.currentTag.name)
                                 .foregroundStyle(.white)
                                 .font(.title2)
                                 .fontWeight(.semibold)
-                            Text(data.timerInterval.timerText)
-                                .foregroundStyle(.white)
-                                .font(
-                                    .system(
-                                        size: UIDevice.current.userInterfaceIdiom == .phone ? 45 : 90,
-                                        weight: .bold
-                                    )
-                                )
-                                .monospacedDigit()
-                                .contentTransition(.numericText(value: data.timerInterval))
-                                .animation(.snappy, value: data.timerInterval.timerText)
                         }
-                    }
-                    .frame(width: circleSize, height: circleSize)
-            
-                case .workSession(let data):
-                    ZStack {
-                        Circle()
-                            .stroke(innserCircleBackground, lineWidth: 4)
-                            .transition(.scale)
-                            .scaleEffect(1.1)
-
-                        Circle()
-                            .trim(from: 0, to: data.progress)
-                            .stroke(param.timerColor.gradient, lineWidth: 4)
-                            .rotationEffect(.init(degrees: 270))
-                            .transition(.scale)
-                            .scaleEffect(1.1)
                         
-                        Circle()
-                            .fill(param.timerColor)
-                            .frame(width: 30, height: 30)
-                            .overlay(content: {
-                                Circle()
-                                    .fill(.white)
-                                    .padding(5)
-                            })
-                            .offset(x: circleSize / 2)
-                            .rotationEffect(.init(degrees: (data.progress * 360) + 270.0))
-                            .scaleEffect(1.1)
-
-                        
-                        Circle()
-                            .fill(data.progress >= 1 ? param.timerColor : innserCircleBackground)
-                            .overlay {
-                                if 0 < data.progress && data.progress < 1 {
-                                    Wave(offset: Angle(degrees: self.waveOffset.degrees), ratio: data.progress)
-                                        .fill(param.timerColor.gradient.opacity(0.8))
-                                        .mask {
-                                            Circle()
-                                        }
-                                    
-                                    Wave(offset: Angle(degrees: self.waveOffset2.degrees), ratio: data.progress)
-                                        .fill(param.timerColor.opacity(0.5))
-                                        .mask {
-                                            Circle()
-                                        }
-                                }
-                            }
-                            .scaleEffect(0.9)
-                        
-                        VStack(spacing: 0) {
-                            let text = data.hasFinishedCountDown ? "+" : ""
-                            Text(text + data.timerInterval.timerText)
-                                .foregroundStyle(.white)
-                                .font(
-                                    .system(
-                                        size: UIDevice.current.userInterfaceIdiom == .phone ? 45 : 90,
-                                        weight: .bold
-                                    )
-                                )
-                                .monospacedDigit()
-                                .contentTransition(.numericText(value: data.timerInterval))
-                                .animation(.snappy, value: data.timerInterval.timerText)
-                        }
-                    }
-                    .frame(width: circleSize, height: circleSize)
-                    
-                case .breakSession(let data):
-                    VStack(spacing: 0) {
                         let text = data.hasFinishedCountDown ? "+" : ""
                         Text(text + data.timerInterval.timerText)
+                            .foregroundStyle(.white)
                             .font(
                                 .system(
                                     size: UIDevice.current.userInterfaceIdiom == .phone ? 45 : 90,
@@ -125,9 +85,25 @@ struct TimerRingView: View {
                             .animation(.snappy, value: data.timerInterval.timerText)
                     }
                 }
+                .frame(width: circleSize, height: circleSize)
+                
+            case .breakSession(let data):
+                VStack(spacing: 0) {
+                    let text = data.hasFinishedCountDown ? "+" : ""
+                    Text(text + data.timerInterval.timerText)
+                        .font(
+                            .system(
+                                size: UIDevice.current.userInterfaceIdiom == .phone ? 45 : 90,
+                                weight: .bold
+                            )
+                        )
+                        .monospacedDigit()
+                        .contentTransition(.numericText(value: data.timerInterval))
+                        .animation(.snappy, value: data.timerInterval.timerText)
+                }
             }
         }
-        .onChange(of: param.isOngoingSession) { oldValue, newValue in
+        .onChange(of: param.isWorkSession) { oldValue, newValue in
             self.timer?.invalidate()
             guard newValue else { return }
             startWaveAnimation()
@@ -173,6 +149,8 @@ extension TimerRingView.TimerRingParam {
         .idle(
             .init(
                 timerInterval: timerSetting.timerType == .countDown ? timerSetting.sessionTimeInterval : 0,
+                progress: 1.0,
+                hasFinishedCountDown: false,
                 currentTag: timerSetting.currentTag ?? .focus()
             )
         )
@@ -181,14 +159,9 @@ extension TimerRingView.TimerRingParam {
 
 extension TimerRingView {
     enum TimerRingParam: Equatable {
-        case idle(IdleData)
+        case idle(WorkSessionData)
         case workSession(WorkSessionData)
         case breakSession(BreakSessionData)
-        
-        struct IdleData: Equatable {
-            var timerInterval: TimeInterval
-            var currentTag: Tag
-        }
         
         struct WorkSessionData: Equatable {
             var timerInterval: TimeInterval
@@ -214,7 +187,29 @@ extension TimerRingView {
             }
         }
         
-        var isOngoingSession: Bool {
+        var isWorkSession: Bool {
+            switch self {
+            case .idle:
+                return false
+            case .workSession:
+                return true
+            case .breakSession:
+                return false
+            }
+        }
+        
+        var isIdleSession: Bool {
+            switch self {
+            case .idle:
+                return true
+            case .workSession:
+                return false
+            case .breakSession:
+                return false
+            }
+        }
+        
+        var isBreakSsession: Bool {
             switch self {
             case .idle:
                 return false
